4ca5ed097cc6d391cce21c6fbe02e1b7de9743ca
Vulture935 <16132157@aru.ac.th>
2026-09-11 00:39:00 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,8 @@ function pickChallengeQuestion(pool, usedIds) {
   return available[Math.floor(Math.random()*available.length)];
 }
 
-const Particles = React.memo(function Particles({ color }: any) { color: string }) {
+const Particles = React.memo(function Particles(props: any) {
+  const color = props.color;
   const pts=useRef([...Array(18)].map(()=>({
     w:Math.random()*2.5+0.5,l:Math.random()*100,t:Math.random()*100,
     d:Math.random()*8+6,delay:Math.random()*6,
